897e6f4a360de589a0312a6a381eb1f52f8e95cd
oscaralvarezz <oscar.alvarezdodani@usp.ceu.es>
2022-10-27 20:13:22 +0200

    update ej6
    probamos otra idea para el determinante de la función

--- ej6.py
+++ ej6.py
@@ -1,3 +1,9 @@
 def determinantedematriz(matriz, n):
     resultado = (n.linalg.det(matriz))
     return (int(round(resultado)))
+
+#intentamos hacer una función que realice el determinante de una matriz    
+import numpy as np
+a = np.array([[65,33,65,33,65],[65,33,65,33,65],[65,33,65,33,65],[65,33,65,33,65],[65,33,65,33,65]])
+d = np.linalg.det(a)
+print(d)
